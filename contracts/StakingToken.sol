abe80b29a00d2aabb4b399fc685b8c4128d31d5f
pudufinance <pudufinance@protonmail.com>
2021-12-08 18:26:25 -0500

    ~puducontract~ test stake/rwrd

---
+++ contracts/StakingToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+// import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+// import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+/**
+* @notice DevToken is a development token that we use to learn how to code solidity 
+* and what BEP-20 interface requires
+*/
+contract StakingToken is ERC20, ERC20Burnable {
+ 
+
+    constructor( 
+      string memory name,
+      string memory symbol,
+      uint256 initialSupply
+    ) ERC20(name, symbol) {
+
+      _mint(msg.sender, initialSupply);
+
+    }
+
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,10 +1,16 @@
 
 // Make sure the DevToken contract is included by requireing it.
-const DevToken = artifacts.require('DevToken')
+const StakingToken = artifacts.require('StakingToken')
+const RewardsToken = artifacts.require('RewardsToken')
+const StakingRewards = artifacts.require('stakingRewards')
 
 // THis is an async function, it will accept the Deployer account, the network, and eventual accounts.
 module.exports = async function (deployer, network, accounts) {
   // await while we deploy the DevToken
-  await deployer.deploy(DevToken, 'Cheese', 'CHEESE', 18, '50000000000000000000000')
-  const devToken = await DevToken.deployed()
+  await deployer.deploy(StakingToken, 'Stake', 'STK', '90000000000000000000000')
+  await deployer.deploy(RewardsToken, 'Rewards', 'RWRD', '9')
+  await deployer.deploy(StakingRewards, '0xA362c5B6865DeDA7891f3EB31BeD650c06A50321', '0x43FD9218B484cDA15c55743756f5e05bd4E88F63')
+  const stakingToken = await StakingToken.deployed()
+  const rewardsToken = await RewardsToken.deployed()
+  const sR = await StakingRewards.deployed()
 }
