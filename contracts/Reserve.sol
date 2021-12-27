7150df5c4594c25e8f305edee7829059bbd607ed
pudufinance <pudufinance@protonmail.com>
2021-12-26 20:20:15 -0500

    [puducontract] rewards

---
+++ contracts/Reserve.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+// import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Reserve is ERC20 {
+    using SafeMath for uint256;
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+
+    constructor(address _stakingToken, address _rewardsToken) {
+        stakingToken = IERC20(_stakingToken);
+        rewardsToken = IERC20(_rewardsToken);
+    
+    }
+    event Redeem(uint256 paid, uint256 recieved);
+
+  // ADDED
+  // still need to prevent anything other than gpudu from 
+  //working with this
+    // Redeem
+    function redeem(uint _amount) external {
+      // transfer staking token  *invoker* to *this contract*
+        rewardsToken.transferFrom(msg.sender, address(this), _amount);
+        uint256 redeemAmount = _amount / 10;
+        stakingToken.transfer(msg.sender, redeemAmount);
+        emit Redeem(_amount, redeemAmount);
+    }
+  
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,14 +3,16 @@
 const StakingToken = artifacts.require('StakingToken')
 const RewardsToken = artifacts.require('RewardsToken')
 const StakingRewards = artifacts.require('stakingRewards')
+const Reserve = artifacts.require('Reserve')
 
 // THis is an async function, it will accept the Deployer account, the network, and eventual accounts.
 module.exports = async function (deployer, network, accounts) {
-  // // await while we deploy the DevToken
+  // // await while we deploy the everything
 
   let add1
   let add2
   let add3
+  let add4
   // Deploy staking token
   await deployer.deploy(StakingToken, 'Stake', 'steak', '900000000000000000000000').then(res => {
     console.log('res 1', res.address)
@@ -26,8 +28,13 @@ module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(StakingRewards, add1, add2).then(res => {
     add3 = res.address
   })
+
+  // Deploy Reserve contract that takes both tokens as args
+  await deployer.deploy(Reserve, add1, add2).then(res => {
+    add4 = res.address
+  })
   // const stakingToken = await StakingToken.deployed()
   // const rewardsToken = await RewardsToken.deployed()
   // const sR = await StakingRewards.deployed()
-  console.log('add1:StakingToken', add1, 'add2:RewardsToken', add2, 'add3:StakingRewards', add3)
+  console.log('add1:StakingToken', add1, 'add2:RewardsToken', add2, 'add3:StakingRewards', add3, 'add4:Reserve', add4)
 }
